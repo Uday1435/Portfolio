63f6d1325b32c41e2ec8fd4bf0ed37e039c3f396
Uday1435 <udayjr.choudhary@gmail.com>
2025-11-25 09:07:00 +0530

    Fix Navbar import issue during deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from './components/navbar.jsx'
+import Navbar from './components/Navbar.jsx'
 import Hero from './components/Hero.jsx'
 import About from './components/About.jsx'
 import Projects from './components/Projects.jsx'
